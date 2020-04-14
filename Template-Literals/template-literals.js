fa1d72096386efa45a08cbed3901e9c2f37c3a57
fazzyA <faz.pak@gmail.com>
2020-04-14 23:58:01 +0500

    template literal assignment

---
+++ Template-Literals/template-literals.js
@@ -0,0 +1,9 @@
+const name = "Faiza Aziz Khan";
+const avatar = 'Web Developer';
+const field = "Web Development";
+const yearsInField = 8;
+const date = new Date();
+const coverLetter= `Hi my name is ${name} and I am a ${avatar} for ${yearsInField} years.
+Today ${date} I want to tell you about myself.`;
+
+console.log(coverLetter);
